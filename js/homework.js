bff37c72030845927634aa77381bf2920e58360d
Tess Leiman <tessleiman@gmail.com>
2024-01-26 14:14:41 -0800

    Initial Commit

---
+++ js/homework.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("#test").hide();
+//     });
+//     $("#test").text("Please work!")
+//     $("#test").addClass("colorr");
+// });
